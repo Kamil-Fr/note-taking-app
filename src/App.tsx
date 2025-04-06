2cb6382ac229c651e1794f098d707e711067173c
Kamil-Fr <kamil.korzeniowski.fr@gmail.com>
2025-04-06 10:23:02 +0200

    Add useLocalStorage file and change App file.

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,30 @@ import { Container } from "react-bootstrap";
 import { Routes, Route, Navigate } from "react-router-dom";
 import { NewNote } from "./NewNote";
 
+export type Note = {
+  id: string
+} & NoteData
+
+export type RawNote = {
+  id: string
+} & RawNoteData
+
+export type RawNoteData = {
+  title: string
+  markdown: string
+  tagIds: string[]
+}
+
+export type NoteData = {
+  title: string
+  markdown: string
+  tags: Tag[]
+}
+
+export type Tag = {
+  id: string
+  label: string
+}
 
 function App() {
   return (
